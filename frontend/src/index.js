53f51f5b4c90a8749ad2098249f21cefb9c9a22d
Sanjjiiev S <sanjjiiev005@gmail.com.com>
2025-07-18 00:51:10 +0530

    Css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 document.body.classList.add('light')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
